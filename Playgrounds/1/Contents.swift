412b19b5b4ab8716d7aa53af19c7a6791cbbab57
Omar Viveros <107490023@comunidad.unam.mx>
2018-09-23 21:20:14 -0500

    Add files via upload

---
+++ Playgrounds/1/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let str = "Hello! "
+let otherStr = "hola!"
+var finalString = str + otherStr
+var string = "cadena1"
+finalString = "ola 2"
+
+let firstName = "Omar"
+let city = "CDMX"
+let welcomeString = "hello \"\(firstName)\", \n welcome to \(city)"
+
+
+let number = 10
+let bool = false
+let name : String

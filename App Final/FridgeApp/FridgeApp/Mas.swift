88c427e6103be657670f38e8a506164fb8414ef0
Omar Viveros <107490023@comunidad.unam.mx>
2018-12-03 10:36:36 -0600

    version 0.4
    Funcionalidad al "botón flotante" para agregar un nuevo elemento

--- App Final/FridgeApp/FridgeApp/Mas.swift
+++ App Final/FridgeApp/FridgeApp/Mas.swift
@@ -29,4 +29,5 @@ class Mas : UIViewController, UITableViewDataSource, UITableViewDelegate{
         
     }
     
+    
 }
---
+++ App Final/FridgeApp/FridgeApp/Producto.swift
@@ -0,0 +1,32 @@
+//
+//  Producto.swift
+//  FridgeApp
+//
+//  Created by Usuario invitado on 12/3/18.
+//  Copyright © 2018 Usuario invitado. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Producto : UIViewController{
+    
+    @IBOutlet weak var inicio: UILabel!
+    
+    func fechaUno(){
+        let today = Date()
+        let month = Calendar.current.component(.month, from: today)
+        let date = Calendar.current.component(.day, from: today)
+        inicio.text = "\(Calendar.current.shortMonthSymbols[month-1]) \(date)"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fechaUno()
+        
+    }
+}
+
+
+
